8c9459fab9d54854ab5963c45cc15746e1e9703e
Qha <qingha821@outlook.com>
2024-09-17 08:39:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,49 +2,49 @@
 
 set -e
 
-npm run preview-build
+//npm run preview-build
 
-rm -rf production-code
-rm -rf ./daemon/dist ./daemon/production
-rm -rf ./panel/dist ./panel/production
+//rm -rf production-code
+//rm -rf ./daemon/dist ./daemon/production
+//rm -rf ./panel/dist ./panel/production
 
-echo "Build daemon..."
-cd daemon
-npm run build
+//echo "Build daemon..."
+//cd daemon
+//npm run build
 
-echo "Build panel..."
-cd ..
-cd panel
-npm run build
+//echo "Build panel..."
+//cd ..
+//cd panel
+//npm run build
 
 echo "Build frontend..."
 cd ..
 cd frontend
 npm run build
 
-echo "Collecting files..."
-cd ..
+//echo "Collecting files..."
+//cd ..
 
-mkdir production-code
-mkdir production-code/daemon
-mkdir production-code/web
-mkdir production-code/web/public
+//mkdir production-code
+//mkdir production-code/daemon
+//mkdir production-code/web
+//mkdir production-code/web/public
 
-mv ./daemon/production/app.js ./production-code/daemon
-mv ./daemon/production/app.js.map ./production-code/daemon
-cp ./daemon/package.json ./production-code/daemon/package.json
-cp ./daemon/package-lock.json ./production-code/daemon/package-lock.json
+//mv ./daemon/production/app.js ./production-code/daemon
+//mv ./daemon/production/app.js.map ./production-code/daemon
+//cp ./daemon/package.json ./production-code/daemon/package.json
+//cp ./daemon/package-lock.json ./production-code/daemon/package-lock.json
 
-mv ./panel/production/app.js ./production-code/web
-mv ./panel/production/app.js.map ./production-code/web
-cp ./panel/package.json ./production-code/web/package.json
-cp ./panel/package-lock.json ./production-code/web/package-lock.json
+//mv ./panel/production/app.js ./production-code/web
+//mv ./panel/production/app.js.map ./production-code/web
+//cp ./panel/package.json ./production-code/web/package.json
+//cp ./panel/package-lock.json ./production-code/web/package-lock.json
 
-mv ./frontend/dist/* ./production-code/web/public
+//mv ./frontend/dist/* ./production-code/web/public
 
-rm -rf ./daemon/dist ./daemon/production
-rm -rf ./panel/dist ./panel/production
-rm -rf ./frontend/dist
+//rm -rf ./daemon/dist ./daemon/production
+//rm -rf ./panel/dist ./panel/production
+//rm -rf ./frontend/dist
 
 echo "------------"
 echo "Compilation completed!"
